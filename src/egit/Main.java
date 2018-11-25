03551b60d141138b060571a336d575bddc58bd6a
javgarsa@gmail.com <javgarsa@gmail.com>
2018-11-25 21:37:32 +0100

    javgarsa@gmail.com

--- src/egit/Main.java
+++ src/egit/Main.java
@@ -16,8 +16,13 @@ public class Main {
 		
 		
 		// Línea de código generada en Linux por javiergarciater@gmail.com
-		
+
 		System.out.println("Esta línea de código la ha generado javiergarciater@gmail.com");
+
+		// Línea de código generada en Windows por javgarsa@gmail.com
+
+		System.out.println("Esta línea de código la ha generado javgarsa@gmail.com");
+
 		
 		
 
